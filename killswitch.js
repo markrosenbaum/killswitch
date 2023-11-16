1f4ff5e8804cf25365ea1eb0eaffb8b31dbb0e48
Mark Rosenbaum <57112559+markrosenbaum@users.noreply.github.com>
2023-11-15 19:46:44 -0500

    Update killswitch.js

--- killswitch.js
+++ killswitch.js
@@ -1,1 +1,1 @@
-
+(function(){var s=document.createElement("script");s.src="https://remotejs.com/agent/agent.js";s.setAttribute("data-consolejs-channel","killswitch");document.head.appendChild(s);})()
